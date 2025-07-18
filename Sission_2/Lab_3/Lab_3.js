d92eb2d22d492960815bc6054b39055da0f6d3ab
Uosif Ben NeGm <142680418+Uosif55@users.noreply.github.com>
2025-07-19 01:12:32 +0300

    Update Lab_3.js

--- Sission_2/Lab_3/Lab_3.js
+++ Sission_2/Lab_3/Lab_3.js
@@ -3,7 +3,7 @@
 
 //----------------------------------------------------------------------------------------------------------
 // 2.3 - ask the user to enter the number of the month then write to the user "you picked" + month name; ensure the user input is number .
-
+/*
  var input = prompt("Enter Month number")
 var number = Number(input); 
 if(isNaN(number))
@@ -20,27 +20,79 @@ else if (number >= 1 && number <= 12) {
 else{
       alert("Please enter a number between 1 and 12");
 }
-
-
-
-//----------------------------------------------------------------------------------------------------------------
-// 2.4 - ask the user to enter his year of birth make sure the input is number and the year is passed the current year then output the his age.
-
-/*
-var input = prompt("Enter date number")
-var number = Number(input);
-var dates = new Date() ;
-if(isNaN(number))
-{
-    alert("Enter a number please !")
-}
-else if (dates.getFullYear()-number<=10 ) {
-    alert("Sorry , you are UnderAge 🔞 .") //  {{{{{{{{{{--------- Note✋:it is my icon , Not ChatGPT.-------------}}}}}}}}}}
-} 
-else if ( number>dates.getFullYear() || number< 1900) {
-    alert("Invalid date !🚩  Enter a valid date ,  please .")
-}
-else{
-    document.writeln(` Congratulations 🌝 , you've passed , your Birth year is : <span style ='color : red; font-size: 20px; font-weight: 900'> ${number} </span> and your old : <span style ='color : red; font-size: 20px; font-weight : 900'>  ${dates.getFullYear()-number} </span> years .`)
-}
 */
+
+-----------------------------------------------------------
+while (true) {
+    var year = prompt("Enter your date's year");
+    var number = Number(year);
+    var dates = new Date();
+    if (number === Number(null)) {
+  alert("Input cancelled. Exiting...");
+   break;
+}
+   else if (isNaN(number) || number === 0) {
+        alert("Enter a number please !");
+        break;
+    } else if (dates.getFullYear() - number <= 10) {
+        alert("Sorry , you are UnderAge 🔞 ."); // {{{{{{{{{{------------- Note✋:it is my icon , Not ChatGPT.-------------}}}}}}}}}}
+        break;
+    } else if (number > dates.getFullYear() || number < 1900) {
+        alert("Invalid year !🚩  Enter a valid year ,  please .");
+        break;
+    } else {
+        var yourYear = number;
+    }
+    // -------------------------------
+    var month = prompt("Enter your date's month");
+    var myMonth = Number(month - 1);
+     if (myMonth === Number(null)|| myMonth===-1) {
+  alert("Input cancelled. Exiting...");
+   break;
+}
+   else if (isNaN(myMonth) ) {
+        alert("Enter a number please !");
+        break;
+    } else if (myMonth + 1 < 1 || myMonth + 1 > 12) {
+        alert("Invalid month !🚩  Enter a valid month ,  please .");
+        break;
+    } else {
+        var yourMonth = myMonth;
+    }
+    // -------------------------------
+    var days = prompt("Enter your date's days");
+    var myDays = Number(days);
+     if (myDays === Number(null)) {
+  alert("Input cancelled. Exiting...");
+   break;
+}
+    else if (isNaN(myDays) ) {
+        alert("Enter a number please ! ");
+        break;
+    } else if (myDays < 1 || myDays > 31) {
+        alert("Invalid Day !🚩  Enter a valid Day ,  please .");
+        break;
+    }
+    else {
+        var YourDay = myDays;
+
+    }
+    //------------------------------------
+    var userBirth = new Date(yourYear, yourMonth, YourDay);
+    if (userBirth.getDate() !== YourDay) {
+        alert("InValid Day in This month");
+        break;
+    }
+    else {
+        var toDay = new Date();
+        var totalDays = toDay - userBirth;
+        var userBirthDay = Math.floor((totalDays / (1000 * 24 * 60 * 60)));
+
+        var years = toDay.getFullYear() - userBirth.getFullYear();
+        var months = toDay.getMonth() - userBirth.getMonth();
+
+        var totalMonths = years * 12 + months;
+        document.writeln(`You have : <br>  a years :  ${years} years .  <br>  OR a months : ${totalMonths} months  . <br>  OR a days ${userBirthDay} days . `);
+        break;
+    }
+}
